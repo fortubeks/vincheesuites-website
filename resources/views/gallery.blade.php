d889bde1d5cbcf2c58f06b0db6b7d5190c1ab00d
Fortune Bekee <fortubeks@gmail.com>
2025-02-24 16:12:43 +0100

    .

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -4,143 +4,143 @@
 <!-- content begin -->
 <div class="no-bottom no-top" id="content">
 
-<div id="top"></div>
+    <div id="top"></div>
 
-<section id="subheader" class="relative jarallax text-light">
-    <img src="almaris/images/background/3.webp" class="jarallax-img" alt="">
-    <div class="container relative z-index-1000">
-        <div class="row justify-content-center">
-            <div class="col-lg-12 text-center">
-                <h1>Gallery</h1>
-                <ul class="crumb">
-                    <li><a href="{{route('home')}}">Home</a></li>
-                    <li class="active">Gallery</li>
-                </ul>
+    <section id="subheader" class="relative jarallax text-light">
+        <img src="almaris/images/background/3.webp" class="jarallax-img" alt="">
+        <div class="container relative z-index-1000">
+            <div class="row justify-content-center">
+                <div class="col-lg-12 text-center">
+                    <h1>Gallery</h1>
+                    <ul class="crumb">
+                        <li><a href="{{route('home')}}">Home</a></li>
+                        <li class="active">Gallery</li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="de-overlay"></div>
-</section>
+        <div class="de-overlay"></div>
+    </section>
 
-<section class="relative lines-deco">
-    <div class="container">
-        <div class="row g-4">
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/1.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/1.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
+    <section class="relative lines-deco">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/1.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/1.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/2.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/2.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/3.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/3.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/4.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/4.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/5.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/5.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/6.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/6.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/7.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/7.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/8.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/8.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-4 text-center">
+                    <a href="almaris/images/gallery/9.jpg" class="image-popup d-block hover">
+                        <div class="relative overflow-hidden rounded-10">
+                            <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
+                                <h4 class="mb-0 hover-scale-in-3">View</h4>
+                            </div>
+                            <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
+                            <img src="almaris/images/gallery/9.jpg" class="img-fluid hover-scale-1-2" alt="">
+                        </div>
+                    </a>
+                </div>
+
             </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/2.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/2.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/3.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/3.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/4.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/4.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/5.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/5.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/6.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/6.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/7.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/7.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/8.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/8.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-lg-4 text-center">
-                <a href="almaris/images/gallery/9.webp" class="image-popup d-block hover">
-                    <div class="relative overflow-hidden rounded-10">
-                        <div class="absolute start-0 w-100 abs-middle fs-36 text-white text-center z-2">
-                            <h4 class="mb-0 hover-scale-in-3">View</h4>
-                        </div> 
-                        <div class="absolute w-100 h-100 top-0 bg-dark hover-op-05"></div>
-                        <img src="almaris/images/gallery/9.webp" class="img-fluid hover-scale-1-2" alt="">
-                    </div>
-                </a>
-            </div>
-
         </div>
-    </div>
-</section>
+    </section>
 </div>
 <!-- content close -->
 @endsection
 <script>
     window.addEventListener('load', function() {
-        
+
     });
 </script>
